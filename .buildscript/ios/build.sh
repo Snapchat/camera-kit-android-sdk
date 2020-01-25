17064c37f9d8969499c54e35a94eff3799dfaf06
Tim Wong <twong@snapchat.com>
2020-01-25 02:22:25 +0000

    [Build][iOS] CI Eject
    Remove all internal refs and copy to eject directory
    https://jira.sc-corp.net/browse/LOOK-63695
    ---
    Automatic squash commit from https://github.sc-corp.net/Snapchat/camera-kit-distribution/pull/14
    Cooled by twong

--- .buildscript/ios/build.sh
+++ .buildscript/ios/build.sh
@@ -10,12 +10,18 @@ set -o pipefail
 set -o xtrace
 
 readonly samples_ios_root="../../samples/ios/CameraKitSample"
+readonly ios_eject_dir=$(mktemp -d -t camerakit-eject-XXXXXXXXXX)
 
 pushd "${samples_ios_root}"
 rm -rf camera-kit-ios-releases
 git clone git@github.sc-corp.net:Snapchat/camera-kit-ios-releases.git
+rm -rf camera-kit-ios-releaes/.git
+sed -i '' 's;git@github.sc-corp.net:Snapchat/camera-kit-ios-releases.git;;g' camera-kit-ios-releases/CameraKit.podspec
 rm -f Podfile
 mv Podfile.ci Podfile
 pod install
 xcodebuild -workspace CameraKitSample.xcworkspace -scheme CameraKitSample -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 8' test
+
+cp -R ../CameraKitSample "${ios_eject_dir}"
+
 popd
