ae201cf25aeb79081285e63f8218fce158129404
Jokūbas Dargis <jokubas.dargis@snapchat.com>
2020-01-25 01:02:38 +0000

    [Build][Android] Add ability to eject project with embedded CameraKit deps
    To distribute CameraKit samples we need to be able to create archive that includes samples apps that have all the necessary dependencies embedded in. This PR creates an "eject" task for the Android sample app - it downloads CameraKit artifacts into a local maven repo and then copies all the project files (with some stripping to remove sensitive data) into an "ejected" directory.
    
    Next step will be to zip this "ejected" directory with documentation and iOS sample into one archive and publish it where needed.
    ---
    Automatic squash commit from https://github.sc-corp.net/Snapchat/camera-kit-distribution/pull/11
    Cooled by jokubas-dargis

--- .buildscript/android/build.sh
+++ .buildscript/android/build.sh
@@ -10,10 +10,12 @@ set -o pipefail
 set -o xtrace
 
 readonly samples_android_root="../../samples/android"
+readonly eject_to_directory=$(mktemp -d -t camerakit-eject-XXXXXXXXXX)
 
 source prepare_build_environment.sh
 echo "Android SDK root: ${ANDROID_SDK_ROOT}"
 
 pushd "${samples_android_root}/kotlin"
 ./gradlew check assembleDebug
+./gradlew eject -PoutputDir="${eject_to_directory}"
 popd
