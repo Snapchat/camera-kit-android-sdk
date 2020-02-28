b8e66fb16a0a1986275dfd25dab1fafd24b1339b
Jokūbas Dargis <jokubas.dargis@snapchat.com>
2020-02-28 18:03:35 +0000

    [Build] Prepare for 0.3.0 dev iteration
    This PR bumps the SDK version to 0.3.3 and updates references to published 0.3.0 libraries.
    ---
    Automatic squash commit from https://github.sc-corp.net/Snapchat/camera-kit-distribution/pull/79
    Cooled by jokubas-dargis

--- .buildscript/ios/build.sh
+++ .buildscript/ios/build.sh
@@ -14,7 +14,7 @@ readonly samples_ios_root="${script_dir}/../../samples/ios"
 readonly program_name=$0
 readonly export_options_plist="${script_dir}/exportOptions.plist"
 readonly archive_path="${script_dir}/archive/CameraKitSample.xcarchive"
-readonly releases_commit="0420f3bba6a4208280c65adf3879ce16aaff6ef6"
+readonly releases_commit="7424003bb2de0327cfae01f66997dca1590a9b54"
 
 usage() {
     echo "usage: ${program_name} [-e --eject-to path]"
