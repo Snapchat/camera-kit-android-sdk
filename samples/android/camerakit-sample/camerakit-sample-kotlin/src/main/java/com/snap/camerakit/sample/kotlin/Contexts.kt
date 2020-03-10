22deb101ab95c19d1cd2d5f1e775ce57944f1a57
Jokūbas Dargis <jokubas.dargis@snapchat.com>
2020-03-10 22:25:38 +0000

    [Fix][Android] Picture/video sharing does not work on Android 10
    Sharing captured picture/video on Android 10 fails due to its scoped storage changes. This PR fixes couple of issues:
    
    1. Add a random UUID based title for images to avoid failure when inserting Bitmap via ContentResolver.
    2. Copy video file into external media exposed by CotentResolver on Android 10 and keep old behavior (no copy) on lower Android versions.
    ---
    Automatic squash commit from https://github.sc-corp.net/Snapchat/camera-kit-distribution/pull/95
    Cooled by jokubas-dargis

--- samples/android/camerakit-sample/camerakit-sample-kotlin/src/main/java/com/snap/camerakit/sample/kotlin/Contexts.kt
+++ samples/android/camerakit-sample/camerakit-sample-kotlin/src/main/java/com/snap/camerakit/sample/kotlin/Contexts.kt
@@ -7,14 +7,20 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.provider.MediaStore
 import java.io.File
+import java.util.UUID
+
+private const val MIME_TYPE_VIDEO_MP4 = "video/mp4"
 
 /**
  * Inserts the provided [bitmap] to media store of external images and opens a sharing intent to the result image URI.
  */
-internal fun Context.shareImageExternally(bitmap: Bitmap) {
-    val url = MediaStore.Images.Media.insertImage(contentResolver, bitmap, null, null)
+internal fun Context.shareImageExternally(bitmap: Bitmap, title: String = UUID.randomUUID().toString()) {
+    // Passing non-null title is necessary on Android 10 otherwise the following call will fail with:
+    // `Failed to build unique file: /storage/emulated/0/Pictures Image image/jpeg`.
+    val url = MediaStore.Images.Media.insertImage(contentResolver, bitmap, title, null)
     if (url != null) {
         val uri = Uri.parse(url)
         val intent = Intent(Intent.ACTION_SEND).apply {
@@ -29,16 +35,35 @@ internal fun Context.shareImageExternally(bitmap: Bitmap) {
  * Inserts the provided [file] to media store of external videos and opens a sharing intent to the result video URI.
  */
 internal fun Context.shareVideoExternally(file: File) {
+    val videoUri: Uri?
     val values = ContentValues().apply {
         put(MediaStore.Video.Media.TITLE, file.name)
-        put(MediaStore.Video.Media.MIME_TYPE, "video/mp4")
-        put(MediaStore.Video.Media.DATA, file.absolutePath)
+        put(MediaStore.Video.Media.MIME_TYPE, MIME_TYPE_VIDEO_MP4)
     }
-    val uri: Uri? = this.contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values)
-    if (uri != null) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        values.apply {
+            put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraKit/")
+            put(MediaStore.Video.Media.IS_PENDING, 1)
+        }
+        val collection = MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        videoUri = contentResolver.insert(collection, values)?.also { uri ->
+            contentResolver.openOutputStream(uri)?.use {
+                file.inputStream().copyTo(it)
+                values.clear()
+                values.put(MediaStore.Video.Media.IS_PENDING, 1)
+                contentResolver.update(uri, values, null, null)
+            }
+        }
+    } else {
+        values.apply {
+            put(MediaStore.Video.Media.DATA, file.absolutePath)
+        }
+        videoUri = contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values)
+    }
+    if (videoUri != null) {
         val intent = Intent(Intent.ACTION_SEND).apply {
-            type = values.getAsString(MediaStore.Video.Media.MIME_TYPE)
-            putExtra(Intent.EXTRA_STREAM, uri)
+            type = MIME_TYPE_VIDEO_MP4
+            putExtra(Intent.EXTRA_STREAM, videoUri)
         }
         startActivity(Intent.createChooser(intent, "Share video"))
     }
