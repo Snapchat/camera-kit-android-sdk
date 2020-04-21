4d860b071f709d284bed56f60a7ed000e1fcfe3d
Tim Wong <twong@snapchat.com>
2020-04-21 20:07:24 +0000

    [Fix][iOS] Original lens should be active on app opening
    ---
    Automatic squash commit from https://github.sc-corp.net/Snapchat/camera-kit-distribution/pull/132
    Cooled by twong

--- .buildscript/ios/build.sh
+++ .buildscript/ios/build.sh
@@ -14,7 +14,7 @@ readonly samples_ios_root="${script_dir}/../../samples/ios"
 readonly program_name=$0
 readonly export_options_plist="${script_dir}/exportOptions.plist"
 readonly archive_path="${script_dir}/archive/CameraKitSample.xcarchive"
-readonly releases_commit="410d04362b53a75a5f7dcb942982743f2949333b"
+readonly releases_commit="243822da415f1835a14d8ccc50d1494af9687d5b"
 
 usage() {
     echo "usage: ${program_name} [-e --eject-to path]"
