b9218e3a2ee9efd20e49b75ae5d40608f2d286af
Tim Wong <twong@snapchat.com>
2020-07-29 17:26:52 +0000

    [Feat][iOS] Add support for dynamic lens launch data
    Bump SDK commit to new SDK/Ref UI commit that contains launch data + example in sample UI
    ---
    Automatic squash commit from https://github.sc-corp.net/Snapchat/camera-kit-distribution/pull/216
    Cooled by twong

--- .buildscript/ios/build.sh
+++ .buildscript/ios/build.sh
@@ -14,7 +14,7 @@ readonly samples_ios_root="${script_dir}/../../samples/ios"
 readonly program_name=$0
 readonly export_options_plist="${script_dir}/exportOptions.plist"
 readonly archive_path="${script_dir}/archive/CameraKitSample.xcarchive"
-readonly releases_commit="7c10a325dcd2e5138e43ea96ab2525d333be74b1"
+readonly releases_commit="5f47cca016efe21383a568759574fb51ce329272"
 
 usage() {
     echo "usage: ${program_name} [-e --eject-to path]"
